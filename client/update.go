5a6872e4875b493e8f8c971dd50ccb3d82f3a99e
Michael Chi <chicong@outlook.com>
2022-09-26 22:27:20 +0800

    Basically working

--- client/update.go
+++ client/update.go
@@ -2,8 +2,15 @@ package client
 
 import (
 	"context"
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
 	"github.com/comoyi/valheim-launcher/log"
-	"strconv"
+	"github.com/comoyi/valheim-launcher/utils/fileutil"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -30,15 +37,29 @@ func update(ctx context.Context, baseDir string, progressChan chan<- struct{}) {
 		return
 	}
 
-	serverFileInfo := &ServerFileInfo{Files: make([]*FileInfo, 0)}
+	//serverFileInfo := &ServerFileInfo{Files: make([]*FileInfo, 0)}
+	//
+	//// test
+	//for i := 0; i < 10; i++ {
+	//	serverFileInfo.Files = append(serverFileInfo.Files, &FileInfo{
+	//		Path:       "/a/b/file-name-" + strconv.Itoa(i+1),
+	//		Hash:       "hash-" + strconv.Itoa(i+1),
+	//		SyncStatus: SyncStatusWait,
+	//	})
+	//}
 
-	// test
-	for i := 0; i < 10; i++ {
-		serverFileInfo.Files = append(serverFileInfo.Files, &FileInfo{
-			Name:       "file-name-" + strconv.Itoa(i+1),
-			Hash:       "hash-" + strconv.Itoa(i+1),
-			SyncStatus: SyncStatusWait,
-		})
+	resp, err := http.Get("http://localhost:10123/files")
+	if err != nil {
+		return
+	}
+	j, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	var serverFileInfo ServerFileInfo
+	err = json.Unmarshal(j, &serverFileInfo)
+	if err != nil {
+		return
 	}
 
 	serverFiles := serverFileInfo.Files
@@ -60,7 +81,11 @@ func update(ctx context.Context, baseDir string, progressChan chan<- struct{}) {
 		default:
 			select {
 			case f := <-syncChan:
-				syncFile(f)
+				err := syncFile(f, baseDir)
+				if err != nil {
+					log.Debugf("sync file failed, fileInfo: %+v, err: %s\n", f, err)
+					return
+				}
 				UpdateInf.Current += 1
 				go func() {
 					progressChan <- struct{}{}
@@ -72,7 +97,77 @@ func update(ctx context.Context, baseDir string, progressChan chan<- struct{}) {
 	}
 }
 
-func syncFile(fileInfo *FileInfo) {
+func syncFile(fileInfo *FileInfo, baseDir string) error {
+	var err error
 	log.Debugf("syncing file info %+v\n", fileInfo)
 	<-time.After(100 * time.Millisecond)
+
+	localPathRaw := fmt.Sprintf("%s%s", baseDir, fileInfo.Path)
+	localPath := filepath.Clean(localPathRaw)
+	log.Debugf("serverPath: %s, localPathRaw: %s, localPath: %s\n", fileInfo.Path, localPathRaw, localPath)
+
+	isExist, err := fileutil.Exists(localPath)
+	if err != nil {
+		return err
+	}
+
+	if fileInfo.Type == TypeDir {
+		if isExist {
+			return nil
+		}
+		err = os.MkdirAll(localPath, os.ModePerm)
+		if err != nil {
+			log.Warnf("os.Mkdir failed, err: %v\n", err)
+			return err
+		}
+	} else {
+		if isExist {
+			f, err := os.Open(localPath)
+			if err != nil {
+				return err
+			}
+			bytes, err := io.ReadAll(f)
+			if err != nil {
+				return err
+			}
+			hashSumRaw := md5.Sum(bytes)
+			hashSum := fmt.Sprintf("%x", hashSumRaw)
+			log.Debugf("file: %s, serverHashSum: %s, hashSum: %s\n", fileInfo.Path, fileInfo.Hash, hashSum)
+
+			if hashSum == fileInfo.Hash {
+				log.Debugf("same file skip , localPath: %s\n", localPath)
+				return nil
+			}
+		}
+		//log.Debugf("remove local file, localPath: %s\n", localPath)
+		//err = os.Remove(localPath)
+		//if err != nil {
+		//	return err
+		//}
+		localDir := filepath.Dir(localPath)
+		err = os.MkdirAll(localDir, os.ModePerm)
+		if err != nil {
+			log.Warnf("os.Mkdir failed, err: %v\n", err)
+			return err
+		}
+
+		file, err := os.Create(localPath)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		resp, err := http.Get("http://localhost:10123/sync?file=" + fileInfo.Path)
+		if err != nil {
+			return err
+		}
+		_, err = io.Copy(file, resp.Body)
+		if err != nil {
+			return err
+		}
+	}
+
+	log.Debugf("[OK]synced file info %+v\n", fileInfo)
+
+	return nil
 }
