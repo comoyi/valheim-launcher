902321849d6db392f44a979e39e1d23ee026dc6b
Michael Chi <chicong@outlook.com>
2022-09-30 04:18:56 +0800

    Config

--- config/config.go
+++ config/config.go
@@ -7,24 +7,21 @@ import (
 	"github.com/spf13/viper"
 	"os"
 	"path/filepath"
-	"strings"
 	"sync"
 )
 
 var Conf Config
 
 type Config struct {
-	DebugLevel string `toml:"debuglevel"`
-	LogLevel   int
-	Protocol   string `toml:"protocol"`
-	Host       string `toml:"host"`
-	Port       int    `toml:"port"`
-	Dir        string `toml:"dir"`
+	LogLevel string `toml:"log_level" mapstructure:"log_level"`
+	Protocol string `toml:"protocol" mapstructure:"protocol"`
+	Host     string `toml:"host" mapstructure:"host"`
+	Port     int    `toml:"port" mapstructure:"port"`
+	Dir      string `toml:"dir" mapstructure:"dir"`
 }
 
 func initDefaultConfig() {
-	viper.SetDefault("debuglevel", "OFF")
-	viper.SetDefault("LogLevel", log.Off)
+	viper.SetDefault("log_level", log.Off)
 	viper.SetDefault("protocol", "http")
 	viper.SetDefault("host", "127.0.0.1")
 	viper.SetDefault("port", 8080)
@@ -37,7 +34,6 @@ func LoadConfig() {
 	viper.SetConfigName("config")
 	viper.SetConfigType("toml")
 	viper.AddConfigPath(".")
-	//viper.AddConfigPath("config")
 	viper.AddConfigPath(fmt.Sprintf("%s%s%s", "$HOME", string(os.PathSeparator), ".valheim-launcher"))
 
 	initDefaultConfig()
@@ -46,24 +42,6 @@ func LoadConfig() {
 	if err != nil {
 		log.Errorf("Read config failed, err: %v\n", err)
 		//return
-	}
-
-	debugLevel := strings.ToUpper(viper.GetString("debuglevel"))
-	switch debugLevel {
-	case "TRACE":
-		viper.Set("LogLevel", log.Trace)
-	case "DEBUG":
-		viper.Set("LogLevel", log.Debug)
-	case "INFO":
-		viper.Set("LogLevel", log.Info)
-	case "WARN":
-		viper.Set("LogLevel", log.Warn)
-	case "ERROR":
-		viper.Set("LogLevel", log.Error)
-	case "OFF":
-		viper.Set("LogLevel", log.Off)
-	default:
-		viper.Set("LogLevel", log.Off)
 	}
 
 	err = viper.Unmarshal(&Conf)
