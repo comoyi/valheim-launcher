1c93824ac1700ce370ff14322a75a11dd0ace495
Michael Chi <chicong@outlook.com>
2022-09-30 19:03:42 +0800

    Announcement

--- client/announcement.go
+++ client/announcement.go
@@ -2,17 +2,41 @@ package client
 
 import (
 	"encoding/json"
+	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/widget"
 	"github.com/comoyi/valheim-launcher/log"
+	"github.com/comoyi/valheim-launcher/util/cryptoutil/md5util"
+	"net/url"
 )
+
+var announcementContent string
+var announcementHash = ""
 
 func refreshAnnouncement(w *widget.Label, box *fyne.Container) {
 	announcement, err := getAnnouncement()
-	if err != nil || announcement == nil || announcement.Content == "" {
+	if err != nil || announcement == nil {
+		box.Hide()
+		return
+	}
+	content := ""
+	if announcement.Content != "" {
+		content = announcement.Content
+		announcementHash = announcement.Hash
+	} else {
+		if announcement.Hash != "" {
+			if announcementHash == announcement.Hash {
+				content = announcementContent
+			}
+		}
+	}
+
+	announcementContent = content
+
+	if content == "" {
 		box.Hide()
 	} else {
-		w.SetText(announcement.Content)
+		w.SetText(content)
 		box.Show()
 	}
 }
@@ -33,7 +57,16 @@ func getAnnouncement() (*Announcement, error) {
 }
 
 func fetchAnnouncement() (string, error) {
-	j, err := httpGet(getFullUrl("/announcement"))
+	finalUrl := ""
+	if announcementContent != "" {
+		hash := md5util.SumString(announcementContent)
+		q := url.Values{}
+		q.Set("hash", hash)
+		finalUrl = fmt.Sprintf("%s%s", getFullUrl("/announcement"), "?"+q.Encode())
+	} else {
+		finalUrl = getFullUrl("/announcement")
+	}
+	j, err := httpGet(finalUrl)
 	if err != nil {
 		return "", err
 	}
